b2b1f8f415f111291345c238d98ea5e3fdf35bb2
Ghislain Bernard <ghislain.bernard@gmail.com>
2024-04-11 15:43:52 +0200

    Insert 'server.js'

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+// ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
+const { chromium } = require('playwright');
+//
+(async () => {
+  const browser = await chromium.launch();
+  //
+  const page = await browser.newPage({ viewport: { height: 768, width: 1024 } });
+  page.on('request', (request) => {
+    console.log(request.url());
+  });
+  //
+  await page.goto('https://playwright.dev/');
+  await page.screenshot({ fullPage: true, path: 'capture.png' });
+  //
+  await browser.close();
+})();
+// ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
