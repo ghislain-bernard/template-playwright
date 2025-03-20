ec89b6a174684ed1f36653fba036792f49daea17
Ghislain Bernard <ghislain.bernard@gmail.com>
2025-03-20 23:29:23 +0100

    Update 'server.js'

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-'use strict';
 // ------------------------------------------- ghislain.bernard@gmail.com ------------------------------------------- //
-const { chromium } = require('playwright');
+import { chromium } from 'playwright';
 //
 (async () => {
   const browser = await chromium.launch();
